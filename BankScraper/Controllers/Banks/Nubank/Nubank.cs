02a2ed9ba84359d4e421d57c2268a4e6284011e3
felipevargas <felipeng.eletrica@gmail.com>
2018-01-11 20:16:40 -0200

    new class to scrapper

--- BankScraper/Controllers/Banks/Nubank/Nubank.cs
+++ BankScraper/Controllers/Banks/Nubank/Nubank.cs
@@ -18,12 +18,12 @@ using BankScraper.Models.Scraper;
 using BankScraper.Models.Comum;
 using BankScraper.Models.Nubank;
 
-namespace BankScraper.Controllers.IntegratedBanks
+namespace BankScraper.Controllers.Banks.Nubank
 {
     /// <summary>
     /// Nubank.
     /// </summary>
-    public class Nubank : IBanks 
+    public class Nubank 
     {
 
         private JObject JNubankToken = new JObject() { };
@@ -31,138 +31,15 @@ namespace BankScraper.Controllers.IntegratedBanks
         private string user_agent = "User-Agent: Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.14.0.1;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|";
         private CookieContainer cookies = new CookieContainer();
 
-
+        //Utils
         Utils util = new Utils();
-
-        /// <summary>
-        /// Gets the account.
-        /// </summary>
-        /// <returns>The account.</returns>
-        /// <param name="login">Login.</param>
-        public  Account GetAccount(Login login)
-        {
-            //Scrapers abstraction 
-            Account account = new Account(){};
-           
-            try
-            {
-                bool status = connect(login);
-                if (status)
-                {
-                    account = NubankToAccount();
-                }
-                
-            }catch(Exception e)
-            {
-                throw e;
-            }
-            finally
-            {
-                disconnect();
-            }
-
-            //Account info
-            account.bank = login.bank;
-            account.number = login.account_number;
-            account.branch = login.account_agency;
-
-            return account;
-        }
-        /// <summary>
-        /// Nubanks process account.
-        /// </summary>
-        /// <returns>The to account.</returns>
-        private Account NubankToAccount()
-        {
-            
-            //Scrapers abstraction 
-            Account account = new Account();
-            JObject jevents = new JObject();
-            JObject bills = new JObject();
-            JObject customer = new JObject();
-            JObject purchases = new JObject();
-
-            //node Object ou element
-            string json_object_element = "amount";
-
-            //get values JSON TOKENS
-            jevents = getEvents();
-            bills = getBillsSummary();
-            customer = getCustomer();
-            purchases = getPurchases();
-
-
-            // **************** events ****************************
-            //Nodes tokens events
-            JToken _customer_id = jevents["customer_id"];
-            JToken _links = jevents["_links"];
-            JToken _updates = jevents["updates"];
-            JToken _events = jevents["events"];
-            JToken _as_of = jevents["as_of"];
-
-            //*************** Costumer ***************************
-            JToken _customer = customer["printed_name"];
-
-            //Transactions
-            JToken _purchases = purchases["transactions"];
-
-
-            //var customer_name = (from s in _customer where s["printed_name"] != null select s["printed_name"]).ToList();
-
-        
-            //get JSON events results
-            //get objects into a list
-            IList<JToken> events = _events.Children().ToList();
-            //Serialize JSON results into .NET objects 
-            List<Events> scraper_events = new List<Events>();
-
-            //PARSER  NUBANK to SCRAPER events
-            foreach (JToken ev in events)
-            {
-                NubankEvents nubank_event = ev.ToObject<NubankEvents>();
-                Events scraper_ev = new Events();
-
-                scraper_ev.name = nubank_event.name;
-                scraper_ev.category = nubank_event.category;
-                scraper_ev.title = nubank_event.title;
-                scraper_ev.amount = util.ConvertValue(nubank_event.amount);
-                scraper_ev.time = nubank_event.time;
-                scraper_ev.message = nubank_event.message;
-                scraper_ev.id = nubank_event.id;
-
-                scraper_events.Add(scraper_ev);
-            }
-
-            //Copy Events
-            account.events = scraper_events;
-
-            //************  Card limit *****************
-            //In main json find in node JSON
-            var events_amounts_changes = (from s in _events where s[json_object_element] != null select s[json_object_element]).ToList();
-            //Last value setting
-            string limite_do_cartao = null;
-
-            //limite do cartao
-            if (events_amounts_changes.Count() > 0)
-            {
-                Double card_amount = Convert.ToDouble(events_amounts_changes.First().Value<string>());
-                //string card Value
-                limite_do_cartao = (card_amount / 100).ToString(".00");
-            }
-            // account.events = nubank_events;
-            //Copy card limit
-            account.personal_credit = limite_do_cartao;
-            //Purchases
-
-            return account;
-        }
 
         /// <summary>
         /// Connect the specified login.
         /// </summary>
         /// <returns>The connect.</returns>
         /// <param name="login">Login.</param>
-        private bool connect(Login login)
+        public bool connect(Login login)
         {
 
             bool status = false;
@@ -223,7 +100,7 @@ namespace BankScraper.Controllers.IntegratedBanks
         /// <summary>
         /// Disconnect this instance.
         /// </summary>
-        private void disconnect()
+        public void disconnect()
         {
             JNubankToken = null;
         }
@@ -311,7 +188,7 @@ namespace BankScraper.Controllers.IntegratedBanks
         /// Gets the bills summary.
         /// </summary>
         /// <returns>The bills summary.</returns>
-        private JObject getBillsSummary()
+        public JObject getBillsSummary()
         {
 
             JObject JNubankBills = new JObject();
@@ -333,7 +210,7 @@ namespace BankScraper.Controllers.IntegratedBanks
         /// Gets the customer informations
         /// </summary>
         /// <returns>The customer.</returns>
-        private JObject getCustomer()
+        public JObject getCustomer()
         {
             JObject JNubank = new JObject();
             //bills_summary
@@ -354,7 +231,7 @@ namespace BankScraper.Controllers.IntegratedBanks
         /// Gets the purchases.
         /// </summary>
         /// <returns>The purchases.</returns>
-        private JObject getPurchases()
+        public JObject getPurchases()
         {
 
             JObject JNubank = new JObject();
