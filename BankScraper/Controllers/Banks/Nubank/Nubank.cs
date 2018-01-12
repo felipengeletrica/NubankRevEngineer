9526625b7847c760bdf26047534d276353896c03
felipevargas <felipeng.eletrica@gmail.com>
2018-01-12 21:25:43 -0200

    adjust size string user_agent

--- BankScraper/Controllers/Banks/Nubank/Nubank.cs
+++ BankScraper/Controllers/Banks/Nubank/Nubank.cs
@@ -28,7 +28,14 @@ namespace BankScraper.Controllers.Banks.Nubank
 
         private JObject JNubankToken = new JObject() { };
         private string token_url = "https://prod-auth.nubank.com.br/api/token";
-        private string user_agent = "User-Agent: Android;Google Nexus 5 - 6.0.0 - API 23 - 1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K eng.buildbot.20160110.195928 test-keys;mov-android-app;6.14.0.1;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|";
+        private string user_agent = 
+            "User-Agent: Android;Google Nexus 5 - 6.0.0 - API 23 - " +
+            "1080x1920;Android;6.0;vbox86p-userdebug 6.0 MRA58K " +
+            "eng.buildbot.20160110.195928 test-keys;mov-android-app;" +
+            "6.14.0.1;en_US;cpu=0|clock=|ram=2052484 kB|espacoSDInterno=12.46 GB|" +
+            "isSmartphone=true|nfc=false|camera=true|cameraFrontal=true|root=true|" +
+            "reconhecimentoVoz=false|resolucao=1080_1776|densidade=3.0|";
+        
         private CookieContainer cookies = new CookieContainer();
 
         //Utils
