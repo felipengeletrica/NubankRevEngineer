cda6c57591963825e7c0576a32743a18f115b7cb
felipevargas <felipeng.eletrica@gmail.com>
2018-01-13 16:36:28 -0200

    removed name

--- BankScraper/Models/Nubank/NubankEvents.cs
+++ BankScraper/Models/Nubank/NubankEvents.cs
@@ -3,16 +3,11 @@
 namespace BankScraper.Models.Nubank
 {
     /// <summary>
-    /// Transaction.
+    /// Events
     /// </summary>
     public class NubankEvents
     {
        
-        /// <summary>
-        /// Gets or sets the name.
-        /// </summary>
-        /// <value>The name.</value>
-        public string name { get; set; }
         public string category { get; set; }
         public string title { get; set; }
         public string amount { get; set; }
@@ -28,16 +23,14 @@ namespace BankScraper.Models.Nubank
         /// <summary>
         /// Initializes a new instance of the <see cref="T:BankScraper.Models.Events"/> class.
         /// </summary>
-        /// <param name="Name">Name.</param>
         /// <param name="Category">Category.</param>
         /// <param name="Title">Title.</param>
         /// <param name="Amount">Amount.</param>
         /// <param name="Time">Time.</param>
         /// <param name="Message">Message.</param>
         /// <param name="Id">Identifier.</param>
-        public NubankEvents(string Name, string Category, string Title, string Amount, string Time, string Message, string Id)
+        public NubankEvents(string Category, string Title, string Amount, string Time, string Message, string Id)
         {
-            name = Name;
             category = Category;
             title = Title;
             amount = Amount;
