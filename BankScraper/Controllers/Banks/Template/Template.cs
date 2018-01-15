924363c63f1bcac8e6f2fbc02f5da833b24eed39
felipevargas <felipeng.eletrica@gmail.com>
2018-01-15 01:24:18 -0200

    add Readme in Template :)

--- BankScraper/Controllers/Banks/Template/Template.cs
+++ BankScraper/Controllers/Banks/Template/Template.cs
@@ -8,5 +8,15 @@ using BankScraper.Models.Comum;
 
 namespace BankScraper.Controllers.Banks.Template
 {
-    
+    //DO NOT REMOVE IT!
+    //Add http methods scrapper for scrapper
+    //Add GET JSON, HTML Others. See in Nubank a form of organizing Nubank.cs
+    //Add in TemplateParser parser for abstrations layers.
+    //Create templates in separate folders, see that there is an abstraction 
+    //layer with templates in common for all banks, if you need to change make 
+    //sure you have not broken compatibility with other banks.
+    //Note that adding property on the templates does not break the code, but rather remove!
+    //ENJOY THE OPEN SOURCE
+
+    //Felipe Vargas Brazil 2018 :)
 }
